df52d8980c2729c827b78a6c146eb5decb3cc773
ramiinberg <rami.inberg@gmail.com>
2022-07-13 11:17:05 +0300

    Changed card component more dynamic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import katieImage from './images/katie-zaferes.png'
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Card />
+      <Card 
+        img={katieImage} 
+        rating={'5.0'} 
+        reviewCount={6} 
+        country='USA' 
+        title='Life Lessons with Katie Zaferes'
+        price={136}
+      />
     </>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,23 @@
-import cardImage from '../images/katie-zaferes.png'
 import star from '../images/star.png'
 import './card.css'
 
-const Card = () => (
+const Card = ({ img, rating, reviewCount, country, title, price}) => (
   <div className='card'>
-    <img src={cardImage} alt="" className='card-image' />
+    <img src={img} alt="" className='card-image' />
     <div className='card-info'>
       <img src={star} alt="small red star" className='card-star' />
-      <p className='card-stats'>5.0 (6) USA</p>
+      <p className='card-stats'>{rating}({reviewCount}) {country}</p>
     </div>
-    <p className='card-title'>Life sessons with Katie Zaferes</p>
-    <p className='card-price'><span className='bold-text'>From $136</span> / person</p>
+    <p className='card-title'>{title}</p>
+    <p className='card-price'><span className='bold-text'>From ${price}</span> / person</p>
   </div>
 )
 
 export default Card
+
+// img={katieImage} 
+//         rating={'5.0'} 
+//         reviewCount={6} 
+//         country='USA' 
+//         title='Life Lessons with Katie Zaferes'
+//         price={136}
